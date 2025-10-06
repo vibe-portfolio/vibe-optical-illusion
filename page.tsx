13e82e08d6989258cce70dd91bd741867016c5ba
vd7 <vdutts7@users.noreply.github.com>
2025-10-06 16:15:38 +0000

    feat: add Neon Maze template project
    Initialize new template project with isometric maze code.
    
    #VERCEL_SKIP
    
    Co-authored-by: vdutts <221189683+vdutts@users.noreply.github.com>

---
+++ page.tsx
@@ -0,0 +1,9 @@
+import NeonIsometricMaze from "./neon-isometric-maze"
+
+export default function Home() {
+  return (
+    <main className="w-full h-screen overflow-hidden bg-black">
+      <NeonIsometricMaze />
+    </main>
+  )
+}
